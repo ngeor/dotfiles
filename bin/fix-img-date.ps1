bb15400aac1dd823eba7c56973b3047a96224b1b
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2023-09-22 20:50:53 +0200

    feat: Support yyyy-MM-dd HHmmss pattern in fix-img-date

--- bin/fix-img-date.ps1
+++ bin/fix-img-date.ps1
@@ -23,6 +23,15 @@ if (Test-Path -Path $filename -PathType Leaf) {
         $day = $Matches.3
         $date = "${year}:${month}:${day} 00:00:00"
         exiftool -AllDates="$date" $filename
+	} elseif ($basename -match '^([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2})([0-9]{2})([0-9]{2})') {
+        $year = $Matches.1
+        $month = $Matches.2
+        $day = $Matches.3
+        $hour = $Matches.4
+        $minute = $Matches.5
+        $second = $Matches.6
+        $date = "${year}:${month}:${day} ${hour}:${minute}:${second}"
+        exiftool -AllDates="$date" $filename		
     } else {
         Write-Error "$basename does not match pattern"
     }
