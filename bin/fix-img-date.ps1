8a73f4d5363ad4c02ee6c41e79271acc135037dc
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2023-09-23 17:29:56 +0200

    feat(fix-img-date): Do not keep a backup of the original photo

--- bin/fix-img-date.ps1
+++ bin/fix-img-date.ps1
@@ -16,13 +16,13 @@ if (Test-Path -Path $filename -PathType Leaf) {
         $minute = $Matches.5
         $second = $Matches.6
         $date = "${year}:${month}:${day} ${hour}:${minute}:${second}"
-        exiftool -AllDates="$date" $filename
+        exiftool -overwrite_original_in_place -AllDates="$date" $filename
     } elseif ($basename -match '^IMG-([0-9]{4})([0-9]{2})([0-9]{2})-') {
         $year = $Matches.1
         $month = $Matches.2
         $day = $Matches.3
         $date = "${year}:${month}:${day} 00:00:00"
-        exiftool -AllDates="$date" $filename
+        exiftool -overwrite_original_in_place -AllDates="$date" $filename
 	} elseif ($basename -match '^([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2})([0-9]{2})([0-9]{2})') {
         $year = $Matches.1
         $month = $Matches.2
@@ -31,7 +31,7 @@ if (Test-Path -Path $filename -PathType Leaf) {
         $minute = $Matches.5
         $second = $Matches.6
         $date = "${year}:${month}:${day} ${hour}:${minute}:${second}"
-        exiftool -AllDates="$date" $filename		
+        exiftool -overwrite_original_in_place -AllDates="$date" $filename
     } else {
         Write-Error "$basename does not match pattern"
     }
