ff678bb57ca350c4f29c68b0f2a6b284929e686f
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2024-02-28 17:35:49 +0100

    Installing vscode keybindings and settings

--- install.cmd
+++ install.cmd
@@ -1,13 +1,17 @@
 @ECHO OFF
 IF /I "%1"=="up" (
-	mklink "%USERPROFILE%\.gitconfig_include" "%CD%\gitconfig_include"
 	mklink "%USERPROFILE%\.bash_aliases" "%CD%\bash_aliases"
+	mklink "%USERPROFILE%\.gitconfig_include" "%CD%\gitconfig_include"
+    mklink "%USERPROFILE%\AppData\Roaming\Code\User\keybindings.json" "%CD%\vscode\keybindings.json"
+    mklink "%USERPROFILE%\AppData\Roaming\Code\User\settings.json" "%CD%\vscode\settings.json"
 	GOTO:EOF
 )
 
 IF /I "%1"=="down" (
-	DEL "%USERPROFILE%\.gitconfig_include"
 	DEL "%USERPROFILE%\.bash_aliases"
+	DEL "%USERPROFILE%\.gitconfig_include"
+    DEL "%USERPROFILE%\AppData\Roaming\Code\User\keybindings.json"
+    DEL "%USERPROFILE%\AppData\Roaming\Code\User\settings.json"
 	GOTO:EOF
 )
 
