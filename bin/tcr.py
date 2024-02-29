cf47033032586066aabc5bd8200d59b8479e5603
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2024-02-29 09:28:09 +0100

    Check if pom.xml exists before running Maven

--- bin/tcr.py
+++ bin/tcr.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python
+# TCR: test && commit || revert, as seen in Kent Beck's article https://medium.com/@kentbeck_7670/test-commit-revert-870bbd756864
 
-# test && commit || revert
-
+import os
+import os.path
 import platform
 import subprocess
 import sys
 
 
 def main():
+    # TODO don't do this on the master branch
+    # TODO don't do this if there are no pending changes
     (test() and commit()) or revert()
 
 
 def test():
+    if os.path.isfile("pom.xml"):
+        return test_with_maven()
+    else:
+        print("No test framework detected")
+        return False
+
+
+def test_with_maven():
     cmd = "mvn.cmd" if platform.system() == "Windows" else "mvn"
     return run([cmd, "-B", "test"])
 
 
 def commit():
-    message = sys.argv[1] if len(sys.argv) >= 1 else "wip"
-    return run(["git", "cm", message])
+    message = sys.argv[1] if len(sys.argv) > 1 else "WIP"
+    return run(["git", "add", "-A"]) and run(["git", "commit", "-m", message])
 
 
 def revert():
@@ -26,9 +37,8 @@ def revert():
     sys.exit(42)
 
 
-def run(args):
-    result = subprocess.run(args)
-    return result.returncode == 0
+def run(cmd):
+    return subprocess.run(cmd).returncode == 0
 
 
 if __name__ == "__main__":
