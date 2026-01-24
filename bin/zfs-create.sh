22d6f925b16c36d0d7fa2dae9c2d269fb7f1f7c9
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2026-01-24 08:39:59 +0100

    feat: Improve zfs-create script

--- bin/zfs-create.sh
+++ bin/zfs-create.sh
@@ -1,21 +1,45 @@
 #!/usr/bin/env bash
+set -ex
+
 POOL=$1
-NAME=$2
+shift
+NAME=$1
+shift
 
 if [[ -z "$POOL" || -z "$NAME" ]]; then
     echo "Usage: $0 POOL NAME"
     exit 1
 fi
 
-# Create a new ZFS data set
-sudo zfs create $POOL/$NAME
+if [ ! -d /$POOL/$NAME ]; then
+    # Create a new ZFS data set
+    sudo zfs create $POOL/$NAME
+fi
 
 # Change group and make it writable to group
 sudo chgrp wheel /$POOL/$NAME
 sudo chmod g+w /$POOL/$NAME
 
 # Sync
-rsync -av ~/$NAME/ /$POOL/$NAME/
+# v verbose
+# r recursive
+# L transform links to referent file/dir
+# p preserve permissions
+# t preserve modification timestamps
+# N preserve creation timestamps - not supported by this version of rsync
+# U preserve access times
+# o preserve owner
+# g preserver group
+# delete: delete extraneous files
+# delete-excluded: also delete excluded files
+# TODO: only for Projects dir, add --exclude='target' \
+sudo rsync -rLptUog --delete --delete-excluded \
+    $* \
+    --exclude='.DS_Store' \
+    --exclude='desktop.ini' \
+    --exclude='.trash' \
+    --exclude='.trashed-*' \
+    ~/$NAME/ /$POOL/$NAME/
 
-# Make initial snapshot
-sudo zfs snapshot $POOL/$NAME@v0
+# Make snapshot
+sudo zfs snapshot $POOL/$NAME@$(date +%Y%m%d-%H%M%S)
