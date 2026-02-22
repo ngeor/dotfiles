9051d73117cd849084c39d63a427cc4654502947
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2026-02-22 18:44:12 +0100

    Add script to copy config files

---
+++ mac/copy-configs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+# Copies configs into an archive folder
+DEST=/Volumes/Lexar128G/configs
+
+if [[ ! -d "$DEST" ]]; then
+    echo "$DEST does not exist"
+    exit 1
+fi
+
+mkdir -p "$DEST/syncthing"
+cp "$HOME/Library/Application Support/Syncthing/config.xml" "$DEST/syncthing"
